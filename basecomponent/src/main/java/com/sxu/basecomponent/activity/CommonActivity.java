f21795df90fab31192dfc12c71af6f892d7b0d61
juhonggang <juhonggang@zhinanmao.com>
2018-07-17 21:04:14 +0800

    修改BaseActivity

---
+++ basecomponent/src/main/java/com/sxu/basecomponent/activity/CommonActivity.java
@@ -0,0 +1,16 @@
+package com.sxu.basecomponent.activity;
+
+import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
+
+/*******************************************************************************
+ * Description: 
+ *
+ * Author: Freeman
+ *
+ * Date: 2018/7/17
+ *
+ * Copyright: all rights reserved by ZhiNanMao.
+ *******************************************************************************/
+
+public class CommonActivity extends SwipeBackActivity {
+}
---
+++ basecomponent/src/main/java/com/sxu/basecomponent/uiwidget/NewDialog.java
@@ -0,0 +1,16 @@
+package com.sxu.basecomponent.uiwidget;
+
+import android.support.v4.app.DialogFragment;
+
+/*******************************************************************************
+ * Description: 
+ *
+ * Author: Freeman
+ *
+ * Date: 2018/7/16
+ *
+ * Copyright: all rights reserved by ZhiNanMao.
+ *******************************************************************************/
+
+public class NewDialog extends DialogFragment {
+}
